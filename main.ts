767a3df029e6e2f5ccec15bf35c6df776229a815
Unknown <rois_r@hotmail.com>
2018-08-25 16:34:49 +0200

    Observable from

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-alert('Works!');
+import { Observable, Observer, from } from 'rxjs';
+
+let numbers = [1,2,3];
+let source = from(numbers);
+
+class MyObserver implements Observer<number>{
+    next(value) {
+        console.log(`Next value in stream: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log(`Complete!`);
+    }
+}
+
+source.subscribe(
+    res => console.log(res)
+);
+
+source.subscribe(new MyObserver());
