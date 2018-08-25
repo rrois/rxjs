162ce889877a49cf42d1a77bc9468d1ba68fcc09
Unknown <rois_r@hotmail.com>
2018-08-25 21:11:23 +0200

    Operador flatMap

--- main.ts
+++ main.ts
@@ -1,27 +1,40 @@
 import { map } from 'rxjs/internal/operators/Map';
 import { filter } from 'rxjs/internal/operators/Filter';
-import { delay } from 'rxjs/operators';
-import { fromEvent } from 'rxjs';
+import { delay, flatMap } from 'rxjs/operators';
+import { Observable,  fromEvent } from 'rxjs';
 
 let output = document.getElementById('output');
 let button = document.getElementById('button');
 
-let click = fromEvent(button, 'click');
+let click: Observable<any> = fromEvent(button, 'click');
 
 let load = function(url: string) {
-  let xhr = new XMLHttpRequest();
+ return Observable.create(observer =>{
+    let xhr = new XMLHttpRequest();
 
-  xhr.addEventListener('load', () => {
-    let movies = JSON.parse(xhr.responseText);
-    movies.forEach(m => {
-      let div = document.createElement('div');
-      div.innerText = m.title;
-      output.appendChild(div);
+    xhr.addEventListener('load', () => {
+      let data = JSON.parse(xhr.responseText);
+      observer.next(data);
+      observer.complete();
     });
-  });
-
-  xhr.open('GET', url);
-  xhr.send();
+  
+    xhr.open('GET', url);
+    xhr.send();
+ });
 };
 
-click.subscribe(e => load('movies.json'), error => console.log(`error: ${error}`), () => console.log(`Complete!`));
+let renderMovies = function(movies) {
+    movies.forEach(m => {
+        let div = document.createElement('div');
+        div.innerText = m.title;
+        output.appendChild(div);
+      });
+}
+
+click.pipe(
+    flatMap(e => load("movies.json"))
+).subscribe(
+    renderMovies,
+    error => console.log(`error: ${error}`),
+    () => console.log(`Complete!`)
+);
