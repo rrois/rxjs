6c5b74261462c26d729ee7f8698e2edb3fb4ad40
Unknown <rois_r@hotmail.com>
2018-08-26 12:33:13 +0200

    Retry observables

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { map } from 'rxjs/internal/operators/Map';
 import { filter } from 'rxjs/internal/operators/Filter';
-import { delay, flatMap } from 'rxjs/operators';
+import { delay, flatMap, retry, retryWhen, scan, takeWhile } from 'rxjs/operators';
 import { Observable,  fromEvent } from 'rxjs';
 
 let output = document.getElementById('output');
@@ -13,16 +13,35 @@ let load = function(url: string) {
     let xhr = new XMLHttpRequest();
 
     xhr.addEventListener('load', () => {
-      let data = JSON.parse(xhr.responseText);
-      observer.next(data);
-      observer.complete();
+        if(xhr.status === 200){
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        } else {
+            observer.error(xhr.statusText);
+        }
     });
   
     xhr.open('GET', url);
     xhr.send();
- });
+ }).pipe(
+     retryWhen(retryStrategy({attempts: 3, delay: 1500}))
+ );
 };
 
+let retryStrategy = function ({attempts = 4, delay: number = 1000}) {
+    return function(errors: Observable<any>){
+        return errors.pipe(
+            scan((accumulator, value) =>{
+                console.log(accumulator, value);
+                return accumulator +1;
+            }, 0),
+            takeWhile(accumulator => accumulator < 4),
+            delay(1000)
+        );
+    }
+
+};
 let renderMovies = function(movies) {
     movies.forEach(m => {
         let div = document.createElement('div');
